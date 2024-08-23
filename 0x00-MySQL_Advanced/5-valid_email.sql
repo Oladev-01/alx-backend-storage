a8cc477dca1393c21ed62d31d6f18040c1046851
olalekan <lekanmojibola@gmail.com>
2024-08-23 22:22:35 +0100

    update

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,17 +1,14 @@
 -- making an update to valid_email
+
+DROP TRIGGER IF EXISTS update_valid_email;
 DELIMITER //
 
 CREATE TRIGGER update_valid_email
 BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
-    -- Update the quantity in the items table based on the newly inserted order
     IF OLD.email <> NEW.email THEN
-        IF OLD.valid_email = 0 THEN
-            SET NEW.valid_email = 1;
-        ELSE
-            SET NEW.valid_email = 0;
-        END IF;
+        SET NEW.valid_email = 0;
     END IF;
 END //
 
