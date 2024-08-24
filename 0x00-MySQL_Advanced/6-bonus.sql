6dbb18a14dd7860c2f7e0eae567cdf66239e24c7
olalekan <lekanmojibola@gmail.com>
2024-08-24 10:48:05 +0100

    procedure

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -2,12 +2,12 @@
 DROP PROCEDURE IF EXISTS AddBonus;
 DELIMITER / /
 
-CREATE PROCEDURE AddBonus(IN user_id INT, IN p_name VARCHAR(255), IN project_score INT)
+CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(255), IN score INT)
 BEGIN
     INSERT INTO projects(project_name)
-    SELECT p_name
-    WHERE NOT EXISTS(SELECT 1 FROM projects WHERE project_name = p_name);
-    INSERT INTO corrections (user_id, project_id, score) VALUES(user_id, (SELECT id from projects WHERE project_name = p_name), project_score);
+    SELECT project_name
+    WHERE NOT EXISTS(SELECT 1 FROM projects WHERE project_name = project_name);
+    INSERT INTO corrections (user_id, project_id, score) VALUES(user_id, (SELECT id from projects WHERE project_name = project_name), score);
 END //
 
 DELIMITER ;
