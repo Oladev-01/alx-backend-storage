9428a3099f142991d777fe73634780e3f92cfb3e
olalekan <lekanmojibola@gmail.com>
2024-08-24 11:00:53 +0100

    procedure

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,24 +1,17 @@
--- comments
-DROP PROCEDURE IF EXISTS AddBonus;
+-- creating a stored procedureDROP PROCEDURE IF EXISTS AddBonus;
 
 DELIMITER / /
 
 CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(255), IN score INT)
 BEGIN
-    DECLARE project_id INT;
-
-    -- Check if the project already exists
-    SELECT id INTO project_id FROM projects WHERE name = project_name;
-
-    -- If the project does not exist, insert it and get the new project_id
-    IF project_id IS NULL THEN
-        INSERT INTO projects (name) VALUES (project_name);
-        SET project_id = LAST_INSERT_ID();
-    END IF;
+    -- Insert the project if it does not exist
+    INSERT INTO projects(name)
+    SELECT project_name
+    WHERE NOT EXISTS(SELECT 1 FROM projects WHERE projects.name = project_name);
 
     -- Insert the correction
     INSERT INTO corrections (user_id, project_id, score)
-    VALUES (user_id, project_id, score);
+    VALUES (user_id, (SELECT id FROM projects WHERE projects.name = project_name), score);
 END //
 
 DELIMITER;
