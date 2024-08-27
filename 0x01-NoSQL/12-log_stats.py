83ecd8d16f4d83fcf1efe4609f971f6f91fb9591
olalekan <lekanmojibola@gmail.com>
2024-08-27 19:40:12 +0100

    show logs

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+from pymongo import MongoClient
+
+
+def show_logs():
+    """show logs"""
+    db = MongoClient()
+    logs = db.logs.nginx
+    print(f"{logs.count_documents({})} logs")
+    print("Methods:")
+    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+
+    for method in methods:
+        print(f"\tmethod {method}: {logs.count_documents({'method': method})}")
+    print(logs.count_documents({'method': 'GET', 'path': '/status'}), end=' ')
+    print("status check")
+
+
+if __name__ == '__main__':
+    show_logs()
