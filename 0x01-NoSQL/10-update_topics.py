480fd0fc708f07d25f14da3593bd7d5634189b28
olalekan <lekanmojibola@gmail.com>
2024-08-27 17:38:48 +0100

    updating

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """updating topics"""
+from typing import List
 from pymongo import collection
 
 
-def update_topics(mongo_collection: collection, name: str, topics: list[str]):
+def update_topics(mongo_collection: collection, name: str, topics: List[str]):
     """update topics"""
     mongo_collection.update_many({"name": name},
                                  {"$set": {"topics": topics}})
