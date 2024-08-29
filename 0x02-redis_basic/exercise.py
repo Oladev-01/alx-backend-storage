4c8f2517ba16c0fd29d557d1f69621ea000134af
olalekan <lekanmojibola@gmail.com>
2024-08-29 18:44:19 +0100

    get from redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """redis"""
 import uuid
 import redis
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -16,3 +16,21 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable]
+            = None) -> Union[str, int, bytes, float]:
+        """get method"""
+        value = self._redis.get(key)
+        if not value:
+            return None
+        if fn:
+            return fn(value)
+        return value
+
+    def get_str(self, key: str) -> Union[str, None]:
+        """convert key to str"""
+        return self._redis.get(key, lambda value: value.decode('utf-8'))
+
+    def get_int(self, key: str) -> Union[int, None]:
+        """convert to int"""
+        return self._redis.get(key, lambda value: int(value))
