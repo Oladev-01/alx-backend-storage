bc318342c5c1fee6da13da5b2fe4d7a0f3318cf8
olalekan <lekanmojibola@gmail.com>
2024-08-30 01:04:57 +0100

    replay

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -19,6 +19,18 @@ def call_history(method: Callable) -> Callable:
     return wraps
 
 
+def replay(method: Callable):
+    """replay"""
+    red_inst = method.__self__._redis
+    input_key = '{}:inputs'.format(method.__qualname__)
+    output_key = '{}:outputs'.format(method.__qualname__)
+    inputs = red_inst.lrange(input_key, 0, -1)
+    outputs = red_inst.lrange(output_key, 0, -1)
+    print(f'{method.__qualname__} was called {len(inputs)} times')
+    for inp, outp in zip(inputs, outputs):
+        print(f"{method.__qualname__}(*{inp.decode('utf-8')}) -> {outp.decode('utf-8')}")  # noqa: E501
+
+
 def count_calls(method: Callable) -> Callable:
     """wrapper"""
     @functools.wraps(method)
@@ -37,6 +49,7 @@ class Cache:
         self._redis.flushdb()
 
     @call_history
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """store method"""
         key = str(uuid.uuid4())
