12d2fde8f788ad88c376e798a763cbae90a6e8e0
olalekan <lekanmojibola@gmail.com>
2024-08-30 07:28:04 +0100

    count cache

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""getting from a page"""
+
+import requests
+import redis
+from typing import Callable
+
+
+r = redis.Redis()
+
+
+def count_cache(method: Callable) -> Callable:
+    """getting count"""
+    def wraps(url: str) -> str:
+        """wrapper"""
+        r.incr(f"count:{url}")
+        page = r.get(url)
+        if page:
+            return page.decode('utf-8')
+        else:
+            page = method(url)
+            r.setex(url, 10, page)
+            return page
+    return wraps
+
+
+@count_cache
+def get_page(url: str) -> str:
+    """function to get page"""
+    page = requests.get(url)
+    return page.text
